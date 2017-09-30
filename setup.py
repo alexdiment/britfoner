f5deb8752030cac149eba3e87ed0314a2c713ba9
Jose Llarena <jose.llarena@gmail.com>
2017-09-30 20:35:08 +0100

    build-fix: undo adding of scipy to setup.py; add scipy install in travis instead

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='jose.llarena@gmail.com',
     url='https://github.com/josellarena/britfoner',
     license='MIT',
-    install_requires=['scipy', 'tensorflow', 'h5py', 'keras', 'scikit-learn'],
+    install_requires=['tensorflow', 'h5py', 'keras', 'scikit-learn'],
     packages=['britfoner','britfoner.recurrentshop', 'britfoner.seq2seq', 'britfoner.recurrentshop.backend'],
     package_data={'britfoner': ['britfone.main.no-stress.2.0.1.csv', '20x32x256x19x48x1.h5']}
 )
