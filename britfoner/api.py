877648a822a818bac583c53e5f1c28b15ca19e4e
Alex Diment <alexdiment@gmail.com>
2019-04-10 13:26:47 +0300

    option to turn off predictions with ML model

--- britfoner/api.py
+++ britfoner/api.py
@@ -23,7 +23,7 @@ _model = model_from('20x32x256x19x48x1.h5')
 _EMPTY_SET = set()
 
 
-def pronounce(word: str) -> Set[Seq]:
+def pronounce(word: str, fallback_to_model=True) -> Set[Seq]:
     '''
     Gives British English pronunciation(s) of word as symbols in the International Phonetic Alphabet
 
@@ -32,6 +32,7 @@ def pronounce(word: str) -> Set[Seq]:
     *input is not validated*
 
     :param word: a non-empty String of length 18 at most, containing characters in [A-Za-z' ]
+    :param fallback_to_model: whether to predict the pronunciation of the unknown words with a ML model
     :return: a set of string tuples representing the pronunciations of ``word``
     '''
 
@@ -41,8 +42,11 @@ def pronounce(word: str) -> Set[Seq]:
     sounds = _dictionary.get(norm_word, None)
 
     if not sounds:
-        bound_word = [bounded(norm_word, MAX_LENGTH)]
-        y_hat = _model.predict([all_encoded(bound_word, _letter_index, reverse=True)])[0]
-        sounds = {most_likely_sequence(y_hat, _inv_phone_index)}
+        if fallback_to_model:
+            bound_word = [bounded(norm_word, MAX_LENGTH)]
+            y_hat = _model.predict([all_encoded(bound_word, _letter_index, reverse=True)])[0]
+            sounds = {most_likely_sequence(y_hat, _inv_phone_index)}
+        else:
+            raise ValueError('Word not found in the dictionary')
 
     return sounds
